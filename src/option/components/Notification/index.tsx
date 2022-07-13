128e439061b9266336b4393c5d0b8069d4fa89e7
Ramil Saavedra <ramilsmcl@gmail.com>
2022-07-14 04:07:37 +0800

    add alt on img / hide when type or message is null

--- src/option/components/Notification/index.tsx
+++ src/option/components/Notification/index.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import success from '../../../../public/images/icons/success.svg';
 import error from '../../../../public/images/icons/error.svg';
 import style from './notification.module.css';
-
-interface NotificationProps {
-  type: 'success' | 'error';
-  message: string;
-}
+import { NotificationProps } from './notification.types';
 
 const Notification: React.FC<NotificationProps> = ({ type, message }) => {
+  if (!type && !message) {
+    return;
+  }
+
   return (
     <div className={style.notification}>
       <img
         className={style['notification-icon']}
-        src={type === 'success' ? success : error}
+        src={type === 'error' ? error : success}
+        alt={type}
       />
       <span className={style[`notification-${type}`]}>{message}</span>
     </div>
