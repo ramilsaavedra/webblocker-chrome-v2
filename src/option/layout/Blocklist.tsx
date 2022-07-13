6f27a0c15750f3a6c3b6917718e5a8f634445f78
Ramil Saavedra <ramilsmcl@gmail.com>
2022-07-14 04:08:40 +0800

    notification on add and remove function

--- src/option/layout/Blocklist.tsx
+++ src/option/layout/Blocklist.tsx
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Button from '../components/Button';
+import NotificationContext from '../context/NotificationContext';
 
 const Blocklist = () => {
+  const { notificationHandler } = useContext(NotificationContext);
   const [URL, setURL] = useState('');
 
   const onAddClick = async () => {
+    if (!URL) {
+      notificationHandler({
+        type: 'error',
+        message: 'Please input URL',
+      });
+      return;
+    }
     await chrome.storage.sync.set({ WebsiteBlockerBlock: URL });
   };
 
   const onClearClick = async () => {
     await chrome.storage.sync.clear();
+    notificationHandler({
+      type: 'success',
+      message: 'Blocklist has been cleared',
+    });
   };
 
   return (
