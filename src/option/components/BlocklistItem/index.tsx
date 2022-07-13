b1de3c79f07e5bb8a6a9bbfe3400d755f14afde2
Ramil Saavedra <ramilsmcl@gmail.com>
2022-07-14 00:39:42 +0800

    init blocklist item component

---
+++ src/option/components/BlocklistItem/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface BlocklistItemProps {
+  children?: React.ReactNode;
+}
+
+const BlocklistItem: React.FC<BlocklistItemProps> = () => {
+  return <div>BlocklistItem</div>;
+};
+
+export default BlocklistItem;
