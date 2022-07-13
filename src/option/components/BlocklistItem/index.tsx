7bdae0d40522db63a42e764f126e088a9260c6db
Ramil Saavedra <ramilsmcl@gmail.com>
2022-07-14 06:25:46 +0800

    blocklistitem component

--- src/option/components/BlocklistItem/index.tsx
+++ src/option/components/BlocklistItem/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import style from './blocklistitem.module.css';
+import icon from '../../../../public/images/icons/error.svg';
 
 interface BlocklistItemProps {
-  children?: React.ReactNode;
+  block: string;
+  onClick: () => void;
 }
 
-const BlocklistItem: React.FC<BlocklistItemProps> = () => {
-  return <div>BlocklistItem</div>;
+const BlocklistItem: React.FC<BlocklistItemProps> = ({ block, onClick }) => {
+  return (
+    <li className={style.blockListItem}>
+      <span className={style['blockListItem-text']}>{block}</span>
+      <button className={style.blockListItemButton} onClick={onClick}>
+        <img src={icon} alt='remove' />
+      </button>
+    </li>
+  );
 };
 
 export default BlocklistItem;
